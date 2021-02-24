a86169ebd2038dfdeab8b9c02c2be087e3e359a4
shramish2057 <shramish4@gmail.com>
2021-02-24 23:16:22 +0100

    making changes

---
+++ src/components/menu.js
@@ -0,0 +1,30 @@
+import "../App.css";
+import { useContext } from "react";
+import { GameStateContext } from "../helpers/contexts";
+
+function Menu() {
+  const { gameState, setGameState, userName, setUserName } = useContext(
+    GameStateContext
+  );
+  return (
+    <div className="Menu">
+      <label>Enter Your Name:</label>
+      <input
+        type="text"
+        placeholder="Name Surname"
+        onChange={(event) => {
+          setUserName(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setGameState("playing");
+        }}
+      >
+        Start Quiz
+      </button>
+    </div>
+  );
+}
+
+export default Menu;
---
+++ src/helpers/questions.js
@@ -0,0 +1,42 @@
+export const Questions = [
+  {
+    prompt: "What's the biggest animal in the world?",
+    optionA: "Elephant",
+    optionB: "Blue Whale",
+    optionC: "Collosal Squid",
+    optionD: "Giraffe",
+    asnwer: "optionB",
+  },
+   {
+    prompt: "What is the capital of Iceland?",
+    optionA: "Vatnajokull",
+    optionB: "Snaefellsjokull",
+    optionC: "Reykavik",
+    optionD: "Oxarafoss",
+    asnwer: "optionC",
+  },
+ {
+    prompt: "Who won FIFA World CUp 2018?",
+    optionA: "Germany",
+    optionB: "France",
+    optionC: "Belgium",
+    optionD: "Brazil",
+    asnwer: "optionB",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    asnwer: "optionD",
+  },
+];
